f7f3acda4cb4cc50d61442d6a826a006ef4ef24a
lucas <stempest@163.com>
2025-03-09 19:38:24 +0800

    feat(ui): add init test
    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -64,7 +64,7 @@ export default function Home() {
             height={16}
           />
           Learn
-        </a>
+       
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -72,6 +72,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <Image
+            
             aria-hidden
             src="/window.svg"
             alt="Window icon"
