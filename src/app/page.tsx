37df1e53a936dee02aefd69cb6a82aea55406b9f
lucas <stempest@163.com>
2025-03-15 19:45:17 +0800

    test1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn11
+          Learn1122
        
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
